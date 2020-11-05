b08e17a347aa669600dc870339ecf3066984e56f
Christopher Morschel <c.morschel@yousee-ar.com>
2020-11-05 14:50:58 +0100

    Add project files.

---
+++ Demo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HoloMetrix.Net.Utilities;
+using HoloMetrix.Net.Remote;
+using HoloMetrix.Net.Remote.SoundIntensity;
+
+namespace Demo_020_2
+{
+    class Demo
+    {
+        private Measurement measurement;
+
+        public async void Test()
+        {
+            var device = BluetoothConnectionUtilities.SelectDeviceFromPicker();
+            if(device == null)
+            {
+                return;
+            }
+
+            var session = BluetoothConnectionUtilities.TryStartRemoteSession(device, BluetoothConnectionUtilities.HoloMetrixHUBService);
+            if(session == null)
+            {
+                return;
+            }
+
+            var siApp = await session.TryLaunchApp<SoundIntensityApp>();
+            if(siApp == null)
+            {
+                return;
+            }
+
+            CuboidObject dut = new CuboidObject(500, 650, 500);
+            CuboidSurface surface = new CuboidSurface(500, 500, 500, 5, 5, 5,
+                new SurfaceState[]
+                {
+                    SurfaceState.Measure, // front
+				    SurfaceState.Measure, // top
+				    SurfaceState.Measure, // left
+				    SurfaceState.Measure, // right
+				    SurfaceState.Solid,   // bottom
+				    SurfaceState.Ignore   // back
+                },
+                new MeasurementMethod[]
+                {
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete
+                });
+
+            measurement = siApp.Setup(dut, surface);
+            if(measurement == null)
+            {
+                return;
+            }
+
+            measurement.SelectSegmentRequested += Measurement_SelectSegmentRequested;
+            measurement.StartMeasurementRequested += Measurement_StartMeasurementRequested;
+
+        }
+
+        private void Measurement_StartMeasurementRequested(object sender, MeasurementEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Measurement_SelectSegmentRequested(object sender, SelectSegmentEventArgs e)
+        {
+            //...
+
+            measurement.SelectSegment(e.SegmentGroupIndex, e.SegmentIndex);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using HoloMetrix.Net.Remote;
+using HoloMetrix.Net.Remote.SoundIntensity;
+
+
+namespace Demo_020_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Test(object sender, RoutedEventArgs e)
+        {
+            CuboidObject dut = new CuboidObject(400, 300, 350);
+            CuboidSurface surf = new CuboidSurface(100, 100, 100, 5, 5, 5,
+                new SurfaceState[]
+                {
+                    SurfaceState.Measure,
+                    SurfaceState.Measure,
+                    SurfaceState.Measure,
+                    SurfaceState.Measure,
+                    SurfaceState.Solid,
+                    SurfaceState.Ignore
+                },
+                new MeasurementMethod[]
+                {
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete,
+                    MeasurementMethod.Discrete
+                });
+
+
+            var hololens = await BluetoothConnectionUtilities.TryConnectToHoloMetrixHub();
+            if(hololens == null)
+            {
+                return;
+            }
+            var siApp = await hololens.TryLaunchApp<SoundIntensityApp>();
+            if (siApp == null)
+            {
+                return;
+            }
+
+            var measurement = await siApp.Setup(dut, surf);
+
+            await measurement.SelectSegment(0, 0);
+        }
+    }
+}
